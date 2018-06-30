46b9396934cd6df968704f32e00d58500d91f0e9
Cagiri <Cagiri@MaroPC-CO>
2018-06-30 15:54:35 +0300

    Subtitles driller

--- src/main/java/com/readitall/main/ReadItAll.java
+++ src/main/java/com/readitall/main/ReadItAll.java
@@ -1,6 +1,9 @@
 package com.readitall.main;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.readitall.constants.ReaderConstants;
 
@@ -8,20 +11,8 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 public class ReadItAll {
-
-	final static String currentDir = System.getProperty("user.dir");
-	static {
-		String jvmArch = System.getProperty("sun.arch.data.model");
-		if (jvmArch.equalsIgnoreCase(ReaderConstants.JVM_ARCH_X64)) {
-			System.load(currentDir + "\\dll\\x64\\gsdll64.dll");
-			System.load(currentDir + "\\dll\\x64\\liblept168.dll");
-			System.load(currentDir + "\\dll\\x64\\libtesseract302.dll");
-		} else {
-			System.load(currentDir + "\\dll\\x86\\gsdll32.dll");
-			System.load(currentDir + "\\dll\\x86\\liblept168.dll");
-			System.load(currentDir + "\\dll\\x86\\libtesseract302.dll");
-		}
-	}
+	
+	private static final String subTitlesFolderPrefix = "C://moviecropper//tempText//";
 
 	public static void main(String[] args) {
 		File imageFile = new File("resource/samples/text.bmp");
@@ -33,6 +24,88 @@ public class ReadItAll {
 			System.out.println(result);
 		} catch (TesseractException e) {
 			System.err.println(e.getMessage());
+		}
+	}
+	
+	public static boolean getSubtitlesFromPng (String folderPath) {
+		
+		File[] listOfFiles = getGrabbedFileList(folderPath);
+		
+		List<ReadedText> subtitleList = getTextList(listOfFiles);
+		
+		return createSubtitleFile(subtitleList);
+	}
+
+	private static List<ReadedText> getTextList(File[] listOfFiles) {
+		List<ReadedText> resultList = new ArrayList<>();
+		for (File file : listOfFiles) {
+			ReadedText readedText = new ReadedText();
+			readedText.setText(readSubtitlesFromPng(file));
+			readedText.setFrameNumber(file.getName().substring(file.getName().indexOf("_")));
+//			readedText.setTime(time); -> This should implement like framenumber / frame of movie
+			if (!readedText.getText().isEmpty()) {
+				resultList.add(readedText);	
+			}
+			
+		}
+		return resultList;
+	}
+
+	private static File[] getGrabbedFileList(String folderPath) {
+		File folder = new File(folderPath);
+		File[] listOfFiles = folder.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.contains(".png");
+			}
+		});
+		return listOfFiles;
+	}
+	
+	private static String readSubtitlesFromPng(File imageFile) {
+		Tesseract instance = new Tesseract();
+		instance.setDatapath("resource/tessdata");
+		String result = "";
+		try {
+		    result = instance.doOCR(imageFile);
+			System.out.println(result);
+		} catch (TesseractException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	private static boolean createSubtitleFile (List<ReadedText> subtitleList) {
+		boolean completed = true;
+		
+		//Should be implemented...
+		
+		return completed;
+	}
+
+	public static class ReadedText {
+		private String text;
+		private String frameNumber;
+		private String time;
+		
+		public String getText() {
+			return text;
+		}
+		public void setText(String text) {
+			this.text = text;
+		}
+		public String getFrameNumber() {
+			return frameNumber;
+		}
+		public void setFrameNumber(String frameNumber) {
+			this.frameNumber = frameNumber;
+		}
+		public String getTime() {
+			return time;
+		}
+		public void setTime(String time) {
+			this.time = time;
 		}
 	}
 }
